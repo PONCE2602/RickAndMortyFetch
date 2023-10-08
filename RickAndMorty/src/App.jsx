cddec875c4fcf4351fad106ca9f397dc17776ee1
PONCE2602 <20690137@tecvalles.mx>
2023-10-08 11:24:29 -0600

    personajes usando fetch

---
+++ RickAndMorty/src/App.jsx
@@ -0,0 +1,10 @@
+import CharacterList from "./components/CharacterList";
+
+function App() {
+  return <div class="bg-dark text-white align-items-center">
+    <h1 class="text-center display-1 py-4">Rick and Morty</h1>
+    <CharacterList />
+  </div>
+}
+
+export default App
